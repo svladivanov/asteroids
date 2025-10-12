44e7f6cdebd2fc08e0a43e1183bd67617e6ea29a
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-12 11:48:24 +0200

    refactoring
    refactor updates and draws into groups instead of single classes

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     while True:
@@ -19,8 +22,9 @@ def main():
                 return
 
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
