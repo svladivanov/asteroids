a6ee37e5eedd5727ed3f45c98098c4154e183a6a
Vlad Ivanov <vlad.ivanov@hometogo.com>
2025-10-12 10:57:04 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import constants
+
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color="black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
