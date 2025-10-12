32b027473b998545bcd7dda5aa36674a827a6316
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-12 13:24:34 +0200

    add shooting for player
    create Shot class, create shoot() method for player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -18,9 +19,11 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     AsteroidField.containers = updateable
     Asteroid.containers = (asteroids, updateable, drawable)
     Player.containers = (updateable, drawable)
+    Shot.containers = (updateable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 import circleshape
 import constants
+from shot import Shot
 
 
 class Player(circleshape.CircleShape):
@@ -26,6 +27,12 @@ class Player(circleshape.CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * constants.PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, constants.SHOT_RADIUS)
+        shot.velocity = (
+            pygame.Vector2(0, 1).rotate(self.rotation) * constants.PLAYER_SHOOT_SPEED
+        )
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -37,3 +44,5 @@ class Player(circleshape.CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
