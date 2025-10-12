91f79870d4731a88518c6fab00aea7b2d0464bef
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-12 11:18:32 +0200

    add player rendering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from player import Player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
 
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
